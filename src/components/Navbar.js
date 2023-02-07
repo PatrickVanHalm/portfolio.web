ae08bcaa3427d2af0469e015a1b5386f1de0174a
Patrick-van-Halm <pvanhalm00@gmail.com>
2023-02-07 17:13:03 +0100

    Hide resume button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
                 <li className="p-4"><a href="#projects" className="hover:underline">Projects</a></li>
                 <li className="p-4"><a href="#portfolio" className="hover:underline">Student Portfolio</a></li>
             </ul>
-            <a href={resume} rel="noreferrer" target="_blank" className="px-6 py-2 shadow-md rounded-md bg-dark-200 hover:bg-dark-100 hover:shadow-xl ml-2">Resume</a>
+            {/* <a href={resume} rel="noreferrer" target="_blank" className="px-6 py-2 shadow-md rounded-md bg-dark-200 hover:bg-dark-100 hover:shadow-xl ml-2">Resume</a> */}
         </div>
     </div>
     )
